542170f1eb148f3e1d7e9ad62207af19ef8c765d
DCYokaze <mote.beats@gmail.com>
2020-07-26 13:01:22 +0700

    first put file to server.

---
+++ WindowsFormsTimer01/WindowsFormsTimer01/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace WindowsFormsTimer01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnSetTimer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.numOfSetToCarry = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.lblETA = new System.Windows.Forms.Label();
+            this.lblCountdown = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnSetTimer
+            // 
+            this.btnSetTimer.Location = new System.Drawing.Point(84, 136);
+            this.btnSetTimer.Name = "btnSetTimer";
+            this.btnSetTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSetTimer.TabIndex = 1;
+            this.btnSetTimer.Text = "sec";
+            this.btnSetTimer.UseVisualStyleBackColor = true;
+            this.btnSetTimer.Click += new System.EventHandler(this.btnSetTimer_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "6",
+            "11",
+            "12"});
+            this.listBox1.Location = new System.Drawing.Point(174, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(29, 52);
+            this.listBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(10, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(57, 22);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Items.AddRange(new object[] {
+            "Pickled",
+            "Vinegar",
+            "Beer"});
+            this.listBox2.Location = new System.Drawing.Point(73, 33);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(63, 84);
+            this.listBox2.TabIndex = 7;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // numOfSetToCarry
+            // 
+            this.numOfSetToCarry.AutoSize = true;
+            this.numOfSetToCarry.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numOfSetToCarry.Location = new System.Drawing.Point(19, 135);
+            this.numOfSetToCarry.Name = "numOfSetToCarry";
+            this.numOfSetToCarry.Size = new System.Drawing.Size(32, 36);
+            this.numOfSetToCarry.TabIndex = 0;
+            this.numOfSetToCarry.Text = "0";
+            this.numOfSetToCarry.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 21);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(91, 150);
+            this.textBox3.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.numOfSetToCarry);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Location = new System.Drawing.Point(265, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 216);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "WeightCalc";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(22, 176);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(117, 21);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "SilverEmbroid";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // lblETA
+            // 
+            this.lblETA.AutoSize = true;
+            this.lblETA.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblETA.Location = new System.Drawing.Point(6, 24);
+            this.lblETA.Name = "lblETA";
+            this.lblETA.Size = new System.Drawing.Size(35, 38);
+            this.lblETA.TabIndex = 11;
+            this.lblETA.Text = "0";
+            // 
+            // lblCountdown
+            // 
+            this.lblCountdown.AutoSize = true;
+            this.lblCountdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountdown.Location = new System.Drawing.Point(6, 82);
+            this.lblCountdown.Name = "lblCountdown";
+            this.lblCountdown.Size = new System.Drawing.Size(35, 38);
+            this.lblCountdown.TabIndex = 12;
+            this.lblCountdown.Text = "0";
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnStop.Location = new System.Drawing.Point(165, 136);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(44, 23);
+            this.btnStop.TabIndex = 13;
+            this.btnStop.Text = "S";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblCountdown);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.btnStop);
+            this.groupBox2.Controls.Add(this.lblETA);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.btnSetTimer);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(232, 219);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Timer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 246);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnSetTimer;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label numOfSetToCarry;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label lblETA;
+        private System.Windows.Forms.Label lblCountdown;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ WindowsFormsTimer01/WindowsFormsTimer01/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTimer01
+{
+    public partial class Form1 : Form
+    {
+        private DateTime m_dateTimeFininsh = DateTime.MinValue;
+        private int m_iRawSec = 0;
+        private int m_iPadding = 30;//sec
+        private Dictionary<string, double> dictFoodSetToWeight;
+        private Dictionary<string, int[]> dictFoodSetToIngred;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+
+            dictFoodSetToWeight = new Dictionary<string, double>
+            {
+                { "Pickled", 0.88 },
+                { "Vinegar", 0.22 },
+                { "Beer", 0.59 },
+                { "", 1.00 }
+            };
+
+            dictFoodSetToIngred = new Dictionary<string, int[]>
+            {
+                { "Pickled", new int[] { 8, 4, 2, 2 } },
+                { "Vinegar", new int[] { 1, 1, 1, 1 } },
+                { "Beer", new int[] { 5, 6, 2, 1 } },
+                { "", new int[] { 1 } }
+            };
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //lblClockDisplay.Text = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+
+            //lblClockDisplay2.Text = DateTime.Now.AddSeconds(30).ToString("HH:mm:ss");
+            //int timeCountDown = int.Parse(lblSS.Text);
+            
+            if (m_iRawSec > 0)
+            {
+                m_iRawSec -= 1;
+
+                //lblCountdown.Text = (m_dateTimeFininsh.Subtract(DateTime.Now).).ToString("HH:mm:ss");
+                DateTime d = DateTime.Now;
+                TimeSpan ts = m_dateTimeFininsh.Subtract(d);
+                //ts.
+                lblCountdown.Text = ts.ToString(@"hh\:mm\:ss");//(m_dateTimeFininsh - d).ToString("HH:mm:ss");
+
+                //lblHH.Text = (m_iRawSec / 3600).ToString();
+                //lblMM.Text = ((m_iRawSec / 60) % 60).ToString();
+                //lblSS.Text = (m_iRawSec % 60).ToString();
+                if(m_iRawSec == 0)
+                {
+                    //this.BringToFront();
+                    this.Activate();
+                    /*
+                    MessageBox.Show("msggg", "captionnnn",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Question);
+                                 */
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            CalculateWeightNumToDo();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CalculateWeightNumToDo();
+        }
+
+        private void CalculateWeightNumToDo()
+        {
+            string sFoodSet = listBox2.Text;
+            double weightLeft;// = int.Parse(textBox2.Text);
+            double weightEachSet = dictFoodSetToWeight[sFoodSet];// = double.Parse(listBox2.Text);
+            int iNumOfSetToCarry = 0;
+            if ( double.TryParse(textBox2.Text, out weightLeft) )
+            {
+                iNumOfSetToCarry = Convert.ToInt32(Math.Floor(weightLeft / weightEachSet));
+            }
+                
+            numOfSetToCarry.Text = iNumOfSetToCarry.ToString();
+
+            textBox3.Text = "";
+            if (iNumOfSetToCarry > 0)
+            {
+                foreach (int i in dictFoodSetToIngred[sFoodSet])
+                {
+                    String s = (iNumOfSetToCarry * i).ToString();
+             
+                    textBox3.AppendText(s);
+                    textBox3.AppendText(Environment.NewLine);
+                }
+                //textBox3.Text = s;
+            }
+
+
+            //System.Console.WriteLine(listBox2.Text);
+            //if(listBox2.SelectedValue.ToString() )
+        }
+
+        private void btnSetTimer_Click(object sender, EventArgs e)
+        {
+            //int numOfTimes = int.Parse(textBox1.Text);
+            //iTimeMultiplier = ;
+            if (int.TryParse(textBox1.Text, out int numOfTimes)
+                && int.TryParse(listBox1.Text, out int iTimeMultiplier))
+            {
+                if (checkBox1.Checked == false)
+                    iTimeMultiplier += 4;
+                m_iRawSec = numOfTimes * iTimeMultiplier + m_iPadding;//got the raw second, keep this number.
+            }
+            m_dateTimeFininsh = DateTime.Now.AddSeconds(m_iRawSec);
+            lblETA.Text = m_dateTimeFininsh.ToString("HH:mm:ss");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            m_iRawSec = 0;
+            m_dateTimeFininsh = DateTime.MinValue;
+            lblETA.Text = m_dateTimeFininsh.ToString("HH:mm:ss");
+            lblCountdown.Text = lblETA.Text;
+        }
+    }
+}
